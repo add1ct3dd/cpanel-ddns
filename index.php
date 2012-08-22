a88d19c9f8faa1b287c923353bd8f6e9b79b9c2e
Joseph W. Becher <jwbecher@gmail.com>
2012-08-21 23:50:50 -0400

    general cleanup
    placed the fetch code in a namespaces function and improved the
    documentation.

--- index.php
+++ index.php
@@ -13,20 +13,34 @@
  */
 require_once 'config.php';
 
-$process = curl_init(CPANEL_DOMAIN . '/xml-api/cpanel?cpanel_xmlapi_module=ZoneEdit&cpanel_xmlapi_func=fetchzone&domain=' . ZONE_DOMAIN);
-curl_setopt($process, CURLOPT_HTTPHEADER, array('Content-Type: application/xml', $additionalHeaders));
-curl_setopt($process, CURLOPT_USERPWD, CPANEL_UN . ":" . CPANEL_PW);
-curl_setopt($process, CURLOPT_TIMEOUT, 30);
-curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
-$return = curl_exec($process);
+/**
+ * Use CURL to query the XML API of cpanel for the DNS zone records
+ * 
+ * Returns the XML response
+ * 
+ * @return xml $xmlZone
+ */
+function cpanel_ddns_zone_fetch() {
+
+    $additionalHeaders = '';
+    $process = curl_init(CPANEL_DOMAIN . '/xml-api/cpanel?cpanel_xmlapi_module=ZoneEdit&cpanel_xmlapi_func=fetchzone&domain=' . ZONE_DOMAIN);
+    curl_setopt($process, CURLOPT_HTTPHEADER, array('Content-Type: application/xml', $additionalHeaders));
+    curl_setopt($process, CURLOPT_USERPWD, CPANEL_UN . ":" . CPANEL_PW);
+    curl_setopt($process, CURLOPT_TIMEOUT, 30);
+    curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
+    $tmpData = curl_exec($process);
 
 // Check if any error occured
-if (curl_errno($process)) {
-    print_r(curl_getinfo($process));
-    die;
+    if (curl_errno($process)) {
+        //TODO: Handle errors cleanly
+        print_r(curl_getinfo($process));
+        die;
+    }
+    $zoneXML = simplexml_load_string($tmpData)->data;
+    return $zoneXML;
 }
 
-$dns_zones = simplexml_load_string($return)->data;
+$dns_zones = cpanel_ddns_zone_fetch();
 
 // Count the number of zone records
 $dns_records_count = count($dns_zones->children()); // PHP < 5.3 version
@@ -42,11 +56,34 @@ for ($i = 0; $i <= $dns_records_count; $i++) {
     }
 }
 
-echo ' % ' . (string) $dns_zones->record[$zone_number_to_update]->name . ' % ';
-echo ' % ' . (string) $dns_zones->record[$zone_number_to_update]->Line . ' % ';
-echo ' % ' . (string) $dns_zones->record[$zone_number_to_update]->ttl . ' % ';
-echo ' % ' . (string) $dns_zones->record[$zone_number_to_update]->address . ' % ';
-echo ' % ' . (string) $dns_zones->record[$zone_number_to_update]->class . ' % ';
+/*
+ * We need to obtain the following values from the current record 
+ * in order to safely update it:
+ * 
+ * Line
+ * ttl
+ * address
+ * 
+ * The following additional values may be used later:
+ * 
+ * name
+ * class
+ * 
+ */
+$zone_record['name'] = (string) $dns_zones->record[$zone_number_to_update]->name;
+echo ' % ' . $zone_record['name'] . ' % ';
+
+$zone_record['Line'] = (string) $dns_zones->record[$zone_number_to_update]->Line;
+echo ' % ' . $zone_record['Line'] . ' % ';
+
+$zone_record['ttl'] = (string) $dns_zones->record[$zone_number_to_update]->ttl;
+echo ' % ' . $zone_record['ttl'] . ' % ';
+
+$zone_record['address'] = (string) $dns_zones->record[$zone_number_to_update]->address;
+echo ' % ' . $zone_record['address'] . ' % ';
+
+$zone_record['class'] = (string) $dns_zones->record[$zone_number_to_update]->class;
+echo ' % ' . $zone_record['class'] . ' % ';
 
 //print_r($dns_zone_records);
 //echo $dns_records_count;
